08afeb824fde33a4702efd66ea83303c77b6774b
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-19 17:53:48 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -81,19 +81,3 @@ submitButton.addEventListener("click", function () {
 
 })
 
-// var mailUtente = prompt("Digita la tua mail")
-// var mailValide = ["sestilialessandro@gmail.com" , "asestilz@gmail5.com" , "cocomeri@gmail.com" , "cani_volanti@gmail.com" , "sanguisughe-spruzzanti_inchiostro@yahoo.it"]
-
-// var mailExists = false
-
-// for (var i = 0; i < mailValide.length; i++) {
-
-//     if(mailValide[i] === mailUtente) {
-//         mailExists = true
-//         alert("La tua Mail è corretta. ")
-//     }
-// }
-
-// if(!mailExists) {
-//     alert("Inserisci una mail valida.")
-// }
